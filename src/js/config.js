626bee3269b0d88cabed4c43d4786cb0c05daad1
Gi0rgiMakharadze <109167550+GiorgiMakharadze@users.noreply.github.com>
2022-10-01 15:55:38 +0400

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+// REUSABLE VARIABLES
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '54e4b4f1-124f-478b-b435-d5ed6fd8db25';
+export const MODAL_CLOSE_SEC = 2.5;
